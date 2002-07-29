cf051d294af12a95f955c057596311e4e67d5def
rrt <rrt>
2002-07-29 22:27:41 +0000

    Generalised daySuffix to ordinalSuffix. Still English-specific :-(

--- std/text/text.lua
+++ std/text/text.lua
@@ -138,3 +138,21 @@ function tostring (x)
   end
   return %_tostring (x)
 end
+
+-- ordinalSuffix: return the English suffix for an ordinal
+--   n: number of the day
+-- returns
+--   s: suffix
+function ordinalSuffix (n)
+  n = imod (n, 100)
+  local d = imod (n, 10)
+  if d == 1 and n ~= 11 then
+    return "st"
+  elseif d == 2 and n ~= 12 then
+    return "nd"
+  elseif d == 3 and n ~= 13 then
+    return "rd"
+  else
+    return "th"
+  end
+end
