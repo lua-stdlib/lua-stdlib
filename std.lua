d9bbc39c47d08466a62b580f955ef925bcd769a4
rrt <rrt>
2002-07-30 22:44:53 +0000

    Remove require for now-defunct time.lua, and tidy up the TODOs.

--- std.lua
+++ std.lua
@@ -6,17 +6,15 @@
 --   function and variable names); library functions should call
 --   error, not die; philosophy of renaming (and hence weak typing,
 --   using raw tables wherever possible)
--- TODO: Make a new SF project LuaStd
 -- TODO: Implement hslibs pretty-printing (sdoc) routines (use .. for
 --   <> and + for <+>), and use in getopt
 -- TODO: Add tests for each function immediately after the function;
 --   this also helps to check module dependencies
--- TODO: precompile and make require check for a .luc version of each
---   file, and load it if it's newer than the .lua version.
+-- TODO: precompile and make require check for a .luc [sic] version of
+--   each file, and load it if it's newer than the .lua version
 
 require "std/patch40.lua"
 require "std/data.lua"
 require "std/text.lua"
-require "std/time.lua"
 require "std/io.lua"
 require "std/debug.lua"
