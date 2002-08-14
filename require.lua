6c8196028a6e78773a1f880c0f88222750d82f0b
rrt <rrt>
2002-08-14 20:27:13 +0000

    Standardize code style, and make changes (mostly to the comments) to prepare for renaming to std.config, as per John's suggestion. This file will only secondarily contain require, and will typically be built into the Lua system anyway. When we move to Lua 5.0, require will disappear, anyway.

--- require.lua
+++ require.lua
@@ -1,4 +1,5 @@
--- Require for Lua 4.0
+-- require for Lua 4.0
+
 -- Adapted from Jay Carlson's version posted to lua-l on 03Dec01
 -- Reuben Thomas 10Jan02
 -- rewrite by John Belmonte
@@ -7,35 +8,39 @@
 --   lua require.lua -f yourfilename.lua
 --   #! /usr/local/bin/lua /usr/local/share/lua/require.lua -f
 
--- I suggest that this file be renamed to "std.config", and define the
--- policy that it must be manually executed (that is, not using require, since
--- this is not a module) before use of stdlib.  This file contains an
--- implementation of require for 4.0, but more importantly it contains
--- system configuration.  Currently the only configuration is the library
--- path, but other configuration options could be added, such as the path
--- separator character (required for platform-independent join function), etc.
---      -John
+-- TODO: Rename this file to "std.config", and say that it must be
+-- executed before using stdlib.
+
 
 --------------------------------------------------------------------------------
 -- stdlib configuration
 
 -- system path of stdlib modules, no trailing separator
-STDLIB_PATH='/usr/local/share/lua'
+STDLIB_PATH = "/usr/local/share/lua"
 
 -- system path separator
---STDLIB_PATH_SEPARATOR='/'
+STDLIB_PATH_SEPARATOR = STDLIB_PATH_SEPARATOR or "/"
 
 --------------------------------------------------------------------------------
+-- Don't edit below here
 
--- add stdlib to module search path
-LUA_PATH = STDLIB_PATH..'/?.lua;'..(LUA_PATH or getenv ('LUA_PATH') or '')
+
+-- Add stdlib to module search path
+LUA_PATH = STDLIB_PATH .. STDLIB_PATH_SEPARATOR .. "/?.lua;" ..
+  (LUA_PATH or getenv ("LUA_PATH") or "")
+
+
+-- require for Lua 4.0
 
 -- Don't define require if it's built-in
-if not require then
-  -- NOTE: 5.0 uses a global _LOADED
-  local loaded = {}
+if require then
+  return
+end
 
-  local exists = function (f)
+_LOADED = {}
+
+local exists =
+  function (f)
     local h = openfile (f, "r")
     if h then
       closefile (h)
@@ -44,27 +49,26 @@ if not require then
     return nil
   end
 
-  require = function (name)
-    if %loaded[name] then
-      return
-    end
-    local path = LUA_PATH or getenv ("LUA_PATH") or '?.lua'
-    local index = 1
-    while index do
-      local s1, s2, dir = strfind (path, "([^;]+)", index)
-      if dir then
-        local filename = gsub (dir, '%?', name)
-        if %exists (filename) then
-          local result = dofile (filename)
-          if not result then
-            error ("error loading package `"..filename.."'")
-          end
-          %loaded[name] = 1
-          return
-        end
-      end
-      index = s2 and (s2 + 1)
-    end
-    error ("could not load package `"..name.."' from path `"..path.."'")
+function require (name)
+  if _LOADED[name] then
+    return
   end
+  local path = LUA_PATH or getenv ("LUA_PATH") or '?.lua'
+  local index = 1
+  while index do
+    local s1, s2, dir = strfind (path, "([^;]+)", index)
+    if dir then
+      local filename = gsub (dir, '%?', name)
+      if %exists (filename) then
+        local result = dofile (filename)
+        if not result then
+          error ("error loading `" .. filename .. "'")
+        end
+        _LOADED[name] = 1
+        return
+      end
+    end
+    index = s2 and (s2 + 1)
+  end
+  error ("could not load `" .. name .. "' from path `" .. path .. "'")
 end
