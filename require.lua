c622bed357b48551ea50786ae8b5849f63d6a03e
rrt <rrt>
2002-08-14 20:14:59 +0000

    John Belmonte's replacement require implementation (5.0-compatible).

--- require.lua
+++ require.lua
@@ -1,53 +1,70 @@
 -- Require for Lua 4.0
 -- Adapted from Jay Carlson's version posted to lua-l on 03Dec01
 -- Reuben Thomas 10Jan02
+-- rewrite by John Belmonte
 
 -- Suggested uses
 --   lua require.lua -f yourfilename.lua
 --   #! /usr/local/bin/lua /usr/local/share/lua/require.lua -f
 
+-- I suggest that this file be renamed to "std.config", and define the
+-- policy that it must be manually executed (that is, not using require, since
+-- this is not a module) before use of stdlib.  This file contains an
+-- implementation of require for 4.0, but more importantly it contains
+-- system configuration.  Currently the only configuration is the library
+-- path, but other configuration options could be added, such as the path
+-- separator character (required for platform-independent join function), etc.
+--      -John
 
--- Don't load if require already present (e.g. in Lua 4.1 or above)
-if require then
-  return
-end
+--------------------------------------------------------------------------------
+-- stdlib configuration
 
-local Private = {loaded = {}}
+-- system path of stdlib modules, no trailing separator
+STDLIB_PATH='/usr/local/share/lua'
 
-local defaultPath = ":/usr/local/share/lua:/usr/share/lua"
-if getenv ("HOME") then
-  defaultPath = defaultPath .. ":" .. getenv ("HOME") .. "/share/lua"
-end
+-- system path separator
+--STDLIB_PATH_SEPARATOR='/'
 
-local Public = {path = LUA_PATH or getenv ("LUA_PATH") or defaultPath}
+--------------------------------------------------------------------------------
 
-Require = Public
+-- add stdlib to module search path
+LUA_PATH = STDLIB_PATH..'/?.lua;'..(LUA_PATH or getenv ('LUA_PATH') or '')
 
-function Public.require (file)
-  local exists =
-    function (f)
-      local h = openfile (f, "r")
-      if h then
-        closefile (h)
-        return 1
-      end
-      return nil
+-- Don't define require if it's built-in
+if not require then
+  -- NOTE: 5.0 uses a global _LOADED
+  local loaded = {}
+
+  local exists = function (f)
+    local h = openfile (f, "r")
+    if h then
+      closefile (h)
+      return 1
     end
-  if not %Private.loaded[file] then
-    local path = {}
-    gsub (%Public.path, "([^:]*)",
-          function (dir)
-            tinsert (%path, dir)
-          end)
-    for i, v in path do
-      %Private.loaded[file] = 1
-      local file = v .. "/" .. file
-      if exists (file) then
-        return dofile (file)
-      end
+    return nil
+  end
+
+  require = function (name)
+    if %loaded[name] then
+      return
     end
-    error ("required file `" .. file .. "' not found")
+    local path = LUA_PATH or getenv ("LUA_PATH") or '?.lua'
+    local index = 1
+    while index do
+      local s1, s2, dir = strfind (path, "([^;]+)", index)
+      if dir then
+        local filename = gsub (dir, '%?', name)
+        if %exists (filename) then
+          local result = dofile (filename)
+          if not result then
+            error ("error loading package `"..filename.."'")
+          end
+          %loaded[name] = 1
+          return
+        end
+      end
+      index = s2 and (s2 + 1)
+    end
+    error ("could not load package `"..name.."' from path `"..path.."'")
   end
 end
-
-require = Public.require
