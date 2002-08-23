731a1bbbe38a903b2628769f130d4233a0e09dd4
rrt <rrt>
2002-08-23 12:11:49 +0000

    Make pickle work for numbers and nil.

--- modules/std.text.pickle.lua
+++ modules/std.text.pickle.lua
@@ -3,24 +3,30 @@
 require "std.text.text"
 
 
--- @func pickle: Convert an object to a string
--- Does not work for functions, userdata or recursive tables
+-- @func pickle: Convert an value to a string
+-- The string can be passed to dostring to retrieve the value
+-- Does not work for recursive tables
 --   @param x: object to pickle
 -- returns
 --   @param s: string that eval (s) is the same value as x
 function pickle (x)
-  local s = ""
-  local rep = stringifier[tag (x)] (x)
-  if type (rep) == "table" then
-    s = s .. "{"
-    for i, v in rep do
-      s = s .. "[" .. pickle (i) .. "]=" .. pickle (v) .. ","
-    end
-    s = s .. "}"
-  elseif type (rep) == "string" then
-    s = format ("%q", rep)
+  local ty = type (x)
+  local s
+  if ty == "nil" then
+    s = "nil"
+  elseif ty == "number" then
+    s = tostring (x)
   else
-    s = tostring (rep)
+    local rep = stringifier[tag (x)] (x)
+    if type (rep) == "table" then
+      s = s .. "{"
+      for i, v in rep do
+        s = s .. "[" .. pickle (i) .. "]=" .. pickle (v) .. ","
+      end
+      s = s .. "}"
+    else
+      s = format ("%q", rep)
+    end
   end
   return s
 end
