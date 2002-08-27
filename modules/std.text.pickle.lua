26f6d9861e9b5bd506ca95a60a70c0cb06326681
rrt <rrt>
2002-08-27 13:45:32 +0000

    Don't need stringify and pickler tables any more, and tostring and pickle can be simplified. They both use tabulator where necessary.

--- modules/std.text.pickle.lua
+++ modules/std.text.pickle.lua
@@ -4,58 +4,33 @@ require "std.data.table"
 require "std.text.text"
 
 
--- @func pickler: table of functions to pickle objects
--- Default method for otherwise unhandled table types
---   {[t] = f, ...} where
---     @param t: tag
---     @param f: function
---       @param x: object of tag t
---       @param [p]: parent object
---     returns
---       @param s: pickle of t
-pickler =
-  defaultTable (function (x)
-                  if tabulator[tag (x)] then
-                    x = tabulator[tag (x)] (x)
-                  end
-                  if type (x) == "table" then
-                    local t = {}
-                    for i, v in x do
-                      t[pickler[tag (i)] (i)] = pickler[tag (v)] (v)
-                    end
-                    return t
-                  else
-                    return x
-                  end
-                end,
-                {
-                  [tag (nil)] = function (x)
-                                  return "nil"
-                                end,
-                  [tag (0)]   = function (x)
-                                  return tostring (x)
-                                end,
-                  [tag ("")]  = function (x)
-                                  return format ("%q", x)
-                                end,
-                })
-
--- @func pickle: Convert an value to a string
+-- @func pickle: Convert a value to a string
 -- The string can be passed to dostring to retrieve the value
 -- Does not work for recursive tables
 --   @param x: object to pickle
 -- returns
 --   @param s: string that eval (s) is the same value as x
 function pickle (x)
-  local rep = pickler[tag (x)] (x)
-  if type (rep) == "table" then
-    local s = "{"
-    for i, v in rep do
-      s = s .. "[" .. tostring (i) .. "]=" .. tostring (v) .. ","
-    end
-    s = s .. "}"
-    return s
+  if type (x) == "nil" then
+    return "nil"
+  elseif type (x) == "number" then
+    return tostring (x)
+  elseif type (x) == "string" then
+    return format ("%q", x)
   else
-    return format ("%q", tostring (rep))
+    if tabulator[tag (x)] then
+      x = tabulator[tag (x)] (x)
+    end
+    if type (x) == "table" then
+      local s, sep = "{", ""
+      for i, v in x do
+        s = s .. sep .. "[" .. pickle (i) .. "]=" .. pickle (v)
+        sep = ","
+      end
+      s = s .. "}"
+      return s
+    else
+      die ("can't pickle " .. tostring (x))
+    end
   end
 end
--- modules/std.text.text.lua
+++ modules/std.text.text.lua
@@ -1,4 +1,4 @@
--- Text processing
+-- Text
 
 require "std.text.regex"
 require "std.data.table"
@@ -19,19 +19,19 @@ function format (...)
   end
 end
 
--- strconcat: Give a name to .. for strings
---   s, t: strings
+-- @func strconcat: Give a name to .. for strings
+--   @param s, t: strings
 -- returns
---   s_: s .. t
+--   @param s_: s .. t
 function strconcat (s, t)
   return s .. t
 end
 
--- strcaps: Capitalise each word in a string
+-- @func strcaps: Capitalise each word in a string
 -- TODO: rewrite for 5.0 using bracket notation
---   s: string
+--   @param s: string
 -- returns
---   s_: capitalised string
+--   @param s_: capitalised string
 function strcaps (s)
   s = gsub (s, "(%w)([%w]*)",
             function (l, ls)
@@ -40,21 +40,21 @@ function strcaps (s)
   return s
 end
 
--- chomp: Remove any final \n from a string
+-- @func chomp: Remove any final \n from a string
 -- TODO: rewrite for 5.0 using bracket notation
---   s: string to process
+--   @param s: string to process
 -- returns
---   s_: processed string
+--   @param s_: processed string
 function chomp (s)
   s = gsub (s, "\n$", "")
   return s
 end
 
--- join: Turn a list of strings into a sep-separated string
---   sep: separator
---   l: list of strings to join
+-- @func join: Turn a list of strings into a sep-separated string
+--   @param sep: separator
+--   @param l: list of strings to join
 -- returns
---   s: joined up string
+--   @param s: joined up string
 function join (sep, l)
   local s = l[1] or ""
   for i = 2, getn (l) do
@@ -63,80 +63,53 @@ function join (sep, l)
   return s
 end
 
--- escapePattern: Escape a string to be used as a pattern
+-- @func escapePattern: Escape a string to be used as a pattern
 -- TODO: rewrite for 5.0 using bracket notation
---   s: string to process
+--   @param s: string to process
 -- returns
---   s_: processed string
+--   @param s_: processed string
 function escapePattern (s)
   s = gsub (s, "(%W)", "%%%1")
   return s
 end
 
--- escapeShell: Escape a string to be used as a shell token
+-- @param escapeShell: Escape a string to be used as a shell token
 -- Quotes spaces, parentheses and \s
 -- TODO: rewrite for 5.0 using bracket notation
---   s: string to process
+--   @param s: string to process
 -- returns
---   s_: processed string
+--   @param s_: processed string
 function escapeShell (s)
   s = gsub (s, "([ %(%)%\\])", "\\%1")
   return s
 end
 
--- stringifier: table of functions to stringify objects
--- Default method for otherwise unhandled table types
--- TODO: make it output in {v1, v2 ..; x1=y1, x2=y2 ..} format; use
--- nexti; show the n field (if any) on the RHS
---   {[t] = f, ...} where
---     t: tag
---     f: function
---       x: object of tag t
---       [p]: parent object
---     returns
---       s: string representation of t
-local _tostring = tostring
-stringifier =
-  defaultTable (function (x)
-                  if tabulator[tag (x)] then
-                    x = tabulator[tag (x)] (x)
-                  end
-                  if type (x) == "table" then
-                    local t = {}
-                    for i, v in x do
-                      t[stringifier[tag (i)] (i)] =
-                        stringifier[tag (v)] (v)
-                    end
-                    return t
-                  else
-                    return %_tostring (x)
-                  end
-                end,
-                {})
-
--- tostring: Extend tostring to work better on tables
---   x: object to convert to string
+-- @func tostring: Extend tostring to work better on tables
+--   @param x: object to convert to string
 -- returns
---   s: string representation
+--   @param s: string representation
+local _tostring = tostring
 function tostring (x)
-  local rep = stringifier[tag (x)] (x)
-  if type (rep) == "table" then
+  if tabulator[tag (x)] then
+    x = tabulator[tag (x)] (x)
+  end
+  if type (x) == "table" then
     local s, sep = "{", ""
-    for i, v in rep do
+    for i, v in x do
       s = s .. sep .. tostring (i) .. "=" .. tostring (v)
       sep = ","
     end
     s = s .. "}"
     return s
   else
-    return rep
+    return %_tostring (x)
   end
 end
 
--- ordinalSuffix: return the English suffix for an ordinal
---   n: number of the day
+-- @func ordinalSuffix: return the English suffix for an ordinal
+--   @param n: number of the day
 -- returns
---   s: suffix
+--   @param s: suffix
 function ordinalSuffix (n)
   n = imod (n, 100)
   local d = imod (n, 10)
