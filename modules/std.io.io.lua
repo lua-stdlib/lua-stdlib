e99b40e4821fb26f0a26ef6095b5e6e40af2e0f8
rrt <rrt>
2002-09-05 15:34:15 +0000

    Add withFileOrHandle, which takes a filename, handle or uses a default handle, opens the file if appropriate, and passes the handle to a given function.
    Use it to generalise readLines and readFile.
    
    It's tempting to generalise writeLine too, but writeLine ("foo", "bar") is
    ambiguous: do we want to write "foo" and "bar" to _OUTPUT, or "bar" to
    file "foo"?

--- modules/std.io.io.lua
+++ modules/std.io.io.lua
@@ -6,9 +6,9 @@ require "std.data.code"
 -- EOL string
 endOfLine = "\n"
 
--- writeLine: Write values adding a newline after each
---   [h]: file handle [_OUTPUT]
---   ...: values to write (as for write)
+-- @func writeLine: Write values adding a newline after each
+--   @param [h]: file handle [_OUTPUT]
+--   @param ...: values to write (as for write)
 function writeLine (h, ...)
   if tag (h) ~= tag (_STDERR) then
     tinsert (arg, 1, h)
@@ -19,59 +19,93 @@ function writeLine (h, ...)
   end
 end
 
--- readLines: Read _INPUT into a list of lines
--- returns
---   line: list of lines
-function readLines ()
-  local line = {}
-  local l
-  repeat
-    l = read ("*l")
-    if l then
-      tinsert (line, l)
+-- @func withFileOrHandle: Generalise a file function
+-- Turns a function accepting a file handle to one that accepts a file
+-- handle or name, defaulting to _INPUT/_OUTPUT, and opens and closes
+-- the given file if necessary.
+--   @param fun: function
+--     @param h: handle
+--     @param ...: other arguments (as below)
+--   returns
+--     @param ...: return values
+--   @param def: default file handle (must be "_INPUT" or "_OUTPUT")
+--   @param [fh]: file handle or name [def]
+--   @param ...: other arguments to fun
+function withFileOrHandle (fun, def, fh, ...)
+  local h = fh or getglobal (def)
+  if type (fh) == "string" then
+    local mode
+    if def == "_INPUT" then
+      mode = "r"
+    elseif def == "_OUTPUT" then
+      mode = "w"
+    else
+      error ("bad default stream " .. def)
     end
-  until l == nil
-  return line
-end
-
--- readFile: Read a file into a string
---   f: file name
--- returns
---   s: string
-function readFile (f)
-  local h = openfile (f, "r")
-  if not h then
-    error ("can't read " .. f)
+    h = openfile (fh, mode)
+    if not h then
+      error ("cannot open " .. fh)
+    end
   end
-  local s = read (h, "*a")
-  closefile (h)
-  return s
+  tinsert (arg, 1, h)
+  local ret = pack (call (fun, arg))
+  if type (fh) == "string" then
+    closefile (h)
+  end
+  return unpack (ret)
 end
+  
+-- @func readLines: Read a file as a list of lines
+--   @param [f]: file handle or name [_INPUT]
+-- returns
+--   @param line: list of lines
+readLines = curry (withFileOrHandle,
+                   function (h)
+                     local line = {}
+                     local l
+                     repeat
+                       l = read (h, "*l")
+                       tinsert (line, l)
+                     until l == nil
+                     line.n = line.n - 1
+                     return line
+                   end,
+                   "_INPUT")
 
--- evalFile: Read a file as a value
---   f: file name
+-- @func readFile: Read a file into a string
+--   @param [f]: file handle or name [_INPUT]
+-- returns
+--   @param s: string
+readFile = curry (withFileOrHandle,
+                  function (h)
+                    return read (h, "*a")
+                  end,
+                  "_INPUT")
+                  
+-- @func evalFile: Read a file as a value
+--   @param [f]: file handle or name [_INPUT]
 -- returns
 --   v: value
 function evalFile (f)
   return eval (readFile (f))
 end
 
--- Change the suffix of a filename
---   from: suffix to change [.-]
---   to: suffix to replace with
---   name: file name to change
+-- @func changeSuffix: Change the suffix of a filename
+--   @param from: suffix to change [.-]
+--   @param to: suffix to replace with
+--   @param name: file name to change
 -- returns
---   name_: file name with new suffix
+--   @param name_: file name with new suffix
 function changeSuffix (from, to, name)
   local from = from or ".-"
   return gsub (name, "%." .. from .. "$", "") .. "." .. to
 end
 
--- Add a suffix to a filename
---   suff: suffix to add
---   name: file name to change
+-- @func addSuffix: Add a suffix to a filename
+--   @param suff: suffix to add
+--   @param name: file name to change
 -- returns
---   name_: file name with new suffix
+--   @param name_: file name with new suffix
 function addSuffix (suff, name)
   return changeSuffix (suff, suff, name)
 end
