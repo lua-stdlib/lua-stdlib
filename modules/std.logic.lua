b7f0ec8cf2c0f684218bd9b14a0393baa6041c25
rrt <rrt>
2002-09-09 21:15:26 +0000

    Shorter implementation of bxor, and bnot thanks to a remark by Paul Hsieh on lual (Message-Id: <0H26009OMQ9J59@mta5.snfc21.pbi.net>).
    Kept band (as the primitive to make bxor) and bor (because de
    Morganising it would involve a call to bnot and two to band, hence
    making it three times slower).

--- modules/std.logic.lua
+++ modules/std.logic.lua
@@ -39,27 +39,11 @@ if band == nil then
   end
 
   function bxor (a, b)
-    local r = 0
-    for i = 0, _INTEGER_BITS - 1 do
-      local x, y = a / 2, b / 2
-      a, b = floor (x), floor (y)
-      if a + b == x + y then
-        r = r + 2 ^ i
-      end
-    end
-    return r
+    return a + b - 2 * band (a, b)
   end
 
   function bnot (a)
-    local r = 0
-    for i = 0, _INTEGER_BITS - 1 do
-      local x = a / 2
-      a = floor (x)
-      if x == a then
-        r = r + 2 ^ i
-      end
-    end
-    return r
+    return bxor (a, -1)
   end
-  
+
 end
