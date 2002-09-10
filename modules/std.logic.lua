cdaa09ffe4be9d8de3007ee2130dc2d955e5dc8f
rrt <rrt>
2002-09-10 12:36:49 +0000

    I was being very dim about bnot. Oops. Roberto pointed it out. I hang my head in shame.

--- modules/std.logic.lua
+++ modules/std.logic.lua
@@ -27,15 +27,7 @@ if band == nil then
   end
 
   function bor (a, b)
-    local r = 0
-    for i = 0, _INTEGER_BITS - 1 do
-      local x, y = a / 2, b / 2
-      a, b = floor (x), floor (y)
-      if a ~= x or b ~= y then
-        r = r + 2 ^ i
-      end
-    end
-    return r
+    return -band (-a - 1, -b - 1) - 1
   end
 
   function bxor (a, b)
@@ -51,15 +43,7 @@ if band == nil then
   end
 
   function bnot (a)
-    local r = 0
-    for i = 0, _INTEGER_BITS - 1 do
-      local x = a / 2
-      a = floor (x)
-      if x == a then
-        r = r + 2 ^ i
-      end
-    end
-    return r
+    return -a - 1
   end
   
 end
