6f2fdd81d4221ddaf4b092bb1c9d86a53010f288
rrt <rrt>
2003-10-18 00:42:39 +0000

    assert lives again! math added to hold math function extensions.

---
+++ modules/std.assert.lua
@@ -0,0 +1,41 @@
+-- Assertions and warnings
+
+require "std.io.io"
+
+
+-- @func assert: Extend to allow formatted arguments
+--   @param v: value
+--   @param ...: arguments for format
+-- returns
+--   @param v: value
+function assert (v, ...)
+  if not v then
+    error (string.format (unpack (arg or {""})))
+  end
+  return v
+end
+
+-- @func warn: Give warning with the name of program and file (if any)
+--   @param ...: arguments for format
+function warn (...)
+  if prog.name then
+    io.stderr:write (prog.name .. ":")
+  end
+  if prog.file then
+    io.stderr:write (prog.file .. ":")
+  end
+  if prog.line then
+    io.stderr:write (tostring (prog.line) .. ":")
+  end
+  if prog.name or prog.file or prog.line then
+    io.stderr:write (" ")
+  end
+  io.writeLine (io.stderr, string.format (unpack (arg)))
+end
+
+-- @func die: Die with error
+--   @param ...: arguments for format
+function die (...)
+  warn (unpack (arg))
+  error (false)
+end
---
+++ modules/std.math.lua
@@ -0,0 +1,28 @@
+-- Math
+
+require "std.list"
+
+
+-- Calculate bits in an integer
+local n, b = 1, 0
+while n < n + 1 do
+  n = n * 2
+  b = b + 1
+end
+math._INTEGER_BITS = b
+
+-- @func math.max: Extend to work on lists
+--   @param (l: list
+--          ( or
+--   @param (v1 ... @param vn: values
+-- returns
+--   @param m: max value
+math.max = listable (math.max)
+
+-- @func math.min: Extend to work on lists
+--   @param (l: list
+--          ( or
+--   @param (v1 ... @param vn: values
+-- returns
+--   @param m: min value
+math.min = listable (math.min)
