46b671028c8e5a4c94de00ca67937cddcd315345
rrt <rrt>
2004-01-27 16:15:12 +0000

    Corrected misnaming of functions and added documentation.

--- modules/std.algorithm.lcs.lua
+++ modules/std.algorithm.lcs.lua
@@ -6,7 +6,31 @@
 
 lcs = {}
 
-function lcs.commonSeqs (a, b, sub, len)
+-- The interface provided by this module is a little unwieldy at first
+-- glance, but is quite easy to use: the best way is probably to
+-- define a wrapper for lcs.longestCommonSubseq for each type on which
+-- you wish to use it.
+--
+-- I used function parameters rather than metamethods because strings
+-- don't have metamethods and there is no standard length metamethod.
+
+
+-- @func lcs.commonSubseqs: find common subsequences
+--   @param a, b: two sequences of type T
+--   @param sub: subscription operator on T
+--     @param s: a sequence of type T
+--     @param i: a number
+--   @returns
+--     @param e: the ith element of s
+--   @param len: length operator on T
+--     @param s: a sequence of type T
+--   @returns
+--     @param l: the length of s
+-- @returns
+--   @param l_: list of common subsequences
+--   @param m: the length of a
+--   @param n: the length of b
+function lcs.commonSubseqs (a, b, sub, len)
   local l, m, n = {}, len (a), len (b)
   for i = m + 1, 1, -1 do
     l[i] = {}
@@ -23,8 +47,26 @@ function lcs.commonSeqs (a, b, sub, len)
   return l, m, n
 end
 
-function lcs.leastCommonSeq (a, b, sub, len, concat, s)
-  local l, m, n = lcs.commonSeqs (a, b, sub, len)
+-- @func lcs.longestCommonSubseq: find the LCS of two sequences
+--   @param a, b: two sequences of some type T
+--   @param sub: subscription operator on T
+--     @param s: a sequence of type T
+--     @param i: a number
+--   @returns
+--     @param e: the ith element of s
+--   @param len: length operator on T
+--     @param s: a sequence of type T
+--   @returns
+--     @param l: the length of s
+--   @concat: concatenation operator on T
+--     @param s, t: two sequences of type T
+--   @returns
+--     @param u: t appended to s
+--   @param s: an empty sequence of type T
+-- @returns
+--   @param s_: the LCS of a and b
+function lcs.longestCommonSubseq (a, b, sub, len, concat, s)
+  local l, m, n = lcs.commonSubseqs (a, b, sub, len)
   local i, j = 1, 1
   while i <= m and j <= n do
     if sub (a, i) == sub (b, j) then
