647ffa26b231b11e7e56ab4df0c1d910a3c222d3
rrt <rrt>
2004-01-28 20:34:44 +0000

    Add primitive way to cope with missing non-standard C libraries, and a TODO to deal with missing C libraries properly.

--- modules/std.lua
+++ modules/std.lua
@@ -1,15 +1,17 @@
--- Lua Standard library
+-- Lua standard library
 
 -- TODO: Write a style guide (indenting/wrapping, capitalisation,
 --   function and variable names); library functions should call
 --   error, not die; OO vs non-OO (a thorny problem)
 -- TODO: Add tests for each function immediately after the function;
 --   this also helps to check module dependencies
+-- TODO: Sort out how to deal with dependencies on C modules
 -- TODO: precompile and make import check for a .luac version of
 --   each file, and load it if it's newer than the .lua version.
 
 require "std.import"
 
+-- Modules that require the standard libraries
 import "std.base"
 import "std.assert"
 import "std.debug"
@@ -20,7 +22,16 @@ import "std.algorithm"
 import "std.string"
 import "std.math"
 import "std.io"
-import "std.bit"
-import "std.rex"
 import "std.set"
 import "std.parser"
+
+
+-- Modules that require non-standard libraries
+
+if type (bit) == "table" then
+  import "std.bit"
+end
+
+if type (rex) == "table" then
+  import "std.rex"
+end
