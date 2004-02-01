43e6921283ff1612bf765d38f3d2eb8f02b8a6be
rrt <rrt>
2004-02-01 20:49:03 +0000

    Massacre the object implementation, reverting to implementing sets as simple tables, which seems to be better for general use. A lot of the code in this file is now non-functional; I'll be making it work later, integrating it with Jamie Webb's code. The module may get folded back into table.

--- modules/std.set.lua
+++ modules/std.set.lua
@@ -4,44 +4,19 @@ import "std.object"
 import "std.table"
 
 
--- Define class
-Set = Object {}
-
-
 -- Primitive methods (access the underlying representation)
 
 -- The representation is a table whose tags are the elements, and
 -- whose values are true.
 
--- @func Set:pairs: Iterator for sets
-function Set:pairs ()
-  return pairs (self.set)
-end
-
--- @func Set:add: Add an element to a set
---   @param e: element
-function Set:add (e)
-  self.set[e] = true
-end
-
--- @func Set:member: Test for membership of a set
---   @param e: element
--- @returns
---   @param f: true if e is in set, false otherwise
-function Set:member (e)
-  return self.set[e] ~= nil
-end
-
--- Set:_clone: Make a list into a set
+-- Set: Make a list into a set
 --   l: list
 -- @returns
 --   s: set
-function Set:_clone (l)
-  local set = table.clone (self)
-  setmetatable (set, set)
-  set.set = {}
+function Set (l)
+  local set = {}
   for _, v in ipairs (l) do
-    set:add (v)
+    set[v] = true
   end
   return set
 end
@@ -49,11 +24,13 @@ end
 
 -- High level methods (no knowledge of representation)
 
+set = {}
+
 -- @func Set:minus: Find the difference of two sets
 --   @param t: set
 -- @returns
 --   @param r: self with elements of t removed
-function Set:minus (t)
+function set.minus (t)
   local r = Set {}
   for e in self:pairs () do
     if not t:member (e) then
@@ -67,7 +44,7 @@ end
 --   @param t: set
 -- @returns
 --   @param r: set intersection of self and t
-function Set:intersect (t)
+function set.intersect (t)
   local r = Set {}
   for e in self:pairs () do
     if t:member (e) then
@@ -81,7 +58,7 @@ end
 --   @param t: set
 -- @returns
 --   @param r: set union of self and t
-function Set:union (t)
+function set.union (t)
   local r = Set {}
   r.set = table.merge (self.set, t.set)
   return r
@@ -91,7 +68,7 @@ end
 --   @param t: set
 -- @returns
 --   @param r: true if self is a subset of t, false otherwise
-function Set:subset (t)
+function set.subset (t)
   for e in self:pairs () do
     if not t:member (e) then
       return false
@@ -105,28 +82,21 @@ end
 --   @param t: set
 -- @returns
 --   @param r: true if s is a proper subset of t, false otherwise
-function Set:propersubset (t)
+function set.propersubset (t)
   return self:subset (t) and not t:subset (self)
-end
-
--- @func Set:empty: Find whether a set is empty
--- @returns
---   r: false if s is empty, true otherwise
-function Set:empty ()
-  return self:subset ({})
 end
 
 -- @func Set:equal: Find whether two sets are equal
 --   @param t: set
 -- @returns
 --   @param r: true if sets are equal, false otherwise
-function Set:equal (t)
+function set.equal (t)
   return self:subset (t) and t:subset (self)
 end
 
 -- Metamethods
-Set.__add = Set.union -- set + table = union
-Set.__sub = Set.minus -- set - table = set difference
-Set.__div = Set.intersect -- set / table = intersection
-Set.__le = Set.subset -- set <= table = subset
-Set.__lt = Set.propersubset -- set < table = proper subset
+-- Set.__add = Set.union -- set + table = union
+-- Set.__sub = Set.minus -- set - table = set difference
+-- Set.__div = Set.intersect -- set / table = intersection
+-- Set.__le = Set.subset -- set <= table = subset
+-- Set.__lt = Set.propersubset -- set < table = proper subset
