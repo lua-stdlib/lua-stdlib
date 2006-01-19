355b680dbc5ef38331526560e459593e3754264b
rrt <rrt>
2006-01-19 21:05:32 +0000

    Improve rex.gsub

--- modules/std/rex.lua
+++ modules/std/rex.lua
@@ -17,12 +17,37 @@ function rex.find (s, p)
 end
 
 -- @function rex.gsub: partial string.gsub for rex
+-- FIXME: Add support for back-refs in replacement string
 --  @param s: string to search
 --  @param p: pattern to find
---  @param f: replacement function
---  @param [n]: maximum number of replacements [infinite]
+--  @param f: replacement function or string
+--  @param [n]: maximum number of replacements [all]
 -- returns
---  @param r: number of replacements made
+--  @param r: string with replacements
+--  @param reps: number of replacements made
 function rex.gsub (s, p, f, n)
-  return rex (p):gmatch (s, f, n)
+  local reg = rex (p)
+  local st = 1
+  local r, reps = {}, 0
+  local from, to, sub
+  if type (f) == "string" then
+    local rep = f
+    f = function ()
+          return rep
+        end
+  end
+  repeat
+    from, to, sub = reg:match (s, st)
+    if from then
+      table.insert (r, string.sub (s, st, from - 1))
+      table.insert (r, f (unpack (sub)))
+      st = to + 1
+      reps = reps + 1
+      if n and reps == n then
+        break
+      end
+    end
+  until not from
+  table.insert (r, string.sub (s, st))
+  return table.concat (r), reps
 end
