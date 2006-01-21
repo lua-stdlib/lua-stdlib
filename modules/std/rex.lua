892475d158b2f241ca34b3dfa3c8aa8920f86365
rrt <rrt>
2006-01-21 04:22:19 +0000

    Make rex.gsub a full gsub for rex.

--- modules/std/rex.lua
+++ modules/std/rex.lua
@@ -2,45 +2,54 @@
 
 -- Default constructor (use PCREs)
 setmetatable (rex, {__call =
-                function (self, p)
-                  return self.newPCRE (p)
+                function (self, p, cf, lo)
+                  return self.newPCRE (p, cf, lo)
                 end})
 
 -- @function rex.find: string.find for rex library
 --   @param s: string to search
 --   @param p: pattern to find
+--   @param [cf]: compile-time flags for the regex
+--   @param [lo]: locale for the regex
+--   @param [ef]: execution flags for the regex
 -- returns
 --   @param from, to: start and end points of match, or nil
 --   @param sub: table of substring matches, or nil if none
-function rex.find (s, p)
-  return rex (p):match (s)
+function rex.find (s, p, cf, lo, ef)
+  return rex (p, cf, lo):match (s, 1, ef)
 end
 
--- @function rex.gsub: partial string.gsub for rex
--- FIXME: Add support for back-refs in replacement string
---  @param s: string to search
---  @param p: pattern to find
---  @param f: replacement function or string
---  @param [n]: maximum number of replacements [all]
+-- @function rex.gsub: string.gsub for rex
+--   @param s: string to search
+--   @param p: pattern to find
+--   @param f: replacement function or string
+--   @param [n]: maximum number of replacements [all]
+--   @param [cf]: compile-time flags for the regex
+--   @param [lo]: locale for the regex
+--   @param [ef]: execution flags for the regex
 -- returns
---  @param r: string with replacements
---  @param reps: number of replacements made
-function rex.gsub (s, p, f, n)
-  local reg = rex (p)
+--   @param r: string with replacements
+--   @param reps: number of replacements made
+function rex.gsub (s, p, f, n, cf, lo, ef)
+  local reg = rex (p, cf, lo)
   local st = 1
   local r, reps = {}, 0
   local from, to, sub
   if type (f) == "string" then
     local rep = f
-    f = function ()
-          return rep
+    f = function (...)
+          local ret = rep
+          for i = 1, table.getn (arg) do
+            ret = string.gsub (ret, "%%" .. tostring (i), arg[i])
+          end
+          return ret
         end
   end
   repeat
-    from, to, sub = reg:match (s, st)
+    from, to, sub = reg:match (s, st, ef)
     if from then
       table.insert (r, string.sub (s, st, from - 1))
-      table.insert (r, f (unpack (sub)))
+      table.insert (r, f (unpack (sub)) or "")
       st = to + 1
       reps = reps + 1
       if n and reps == n then
