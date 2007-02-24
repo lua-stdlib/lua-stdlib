28c5c37080816ad067ea181b1ca6f97a1340d37d
rrt <rrt>
2007-02-24 19:08:20 +0000

    Set had been left rather broken; fix it up.

--- modules/set.lua
+++ modules/set.lua
@@ -6,85 +6,97 @@ require "object"
 require "table_ext"
 
 
--- Primitive methods (access the underlying representation)
+-- Primitive methods (know about representation)
 
 -- The representation is a table whose tags are the elements, and
 -- whose values are true.
 
 -- @func member: Say whether an element is in a set
+--   @param s: set
 --   @param e: element
 -- @returns
 --   @param f: true if e is in set, false otherwise
-function member (e)
-  return self[e] == true
+function member (s, e)
+  return s[e] == true
 end
 
--- @func add: Add an element to a set
+-- @func insert: Insert an element to a set
+--   @param s: set
 --   @param e: element
-function add (e)
-  self[e] = true
+function insert (s, e)
+  s[e] = true
 end
 
 -- @func new: Make a list into a set
 --   @param l: list
 -- @returns
 --   @param s: set
+metatable = {}
 function new (l)
-  local s = {}
-  for _, v in ipairs (l) do
-    s:add (true)
+  local s = setmetatable ({}, metatable)
+  for _, e in ipairs (l) do
+    insert (s, e)
   end
   return s
 end
 
+-- @func elements: Iterator for sets
+-- TODO: Make the iterator return only the key
+elements = pairs
 
--- High level methods (no knowledge of representation)
 
--- @func minus: Find the difference of two sets
---   @param t: set
+-- High level methods (representation unknown)
+
+-- @func difference: Find the difference of two sets
+--   @param s, t: sets
 -- @returns
 --   @param r: self with elements of t removed
-function minus (t)
+function difference (s, t)
   local r = new {}
-  for e in self:pairs () do
-    if not t:member (e) then
-      r:add (e)
+  for e in elements (s) do
+    if not member (s, e) then
+      insert (r, e)
     end
   end
   return r
 end
 
--- @func intersect: Find the intersection of two sets
---   @param t: set
+-- @func intersection: Find the intersection of two sets
+--   @param s, t: sets
 -- @returns
 --   @param r: set intersection of self and t
-function intersect (t)
+function intersection (s, t)
   local r = new {}
-  for e in self:pairs () do
-    if t:member (e) then
-      r:add (e)
+  for e in elements (s) do
+    if member (t, e) then
+      insert (r, e)
     end
   end
   return r
 end
 
 -- @func union: Find the union of two sets
---   @param t: set
+--   @param s, t: sets
 -- @returns
---   @param r: set union of self and t
-function union (t)
+--   @param r: set union of s and t
+function union (s, t)
   local r = new {}
-  r.set = table.merge (self.set, t.set)
+  for e in elements (s) do
+    insert (r, e)
+  end
+  for e in elements (t) do
+    insert (r, e)
+  end
   return r
 end
 
 -- @func subset: Find whether one set is a subset of another
---   @param t: set
+--   @param s, t: sets
 -- @returns
 --   @param r: true if self is a subset of t, false otherwise
-function subset (t)
-  for e in self:pairs () do
-    if not t:member (e) then
+function subset (s, t)
+  for e in elements (s) do
+    if not member (t, e) then
       return false
     end
   end
@@ -93,29 +105,29 @@ end
 
 -- @func propersubset: Find whether one set is a proper subset of
 -- another
---   @param t: set
+--   @param s, t: sets
 -- @returns
 --   @param r: true if s is a proper subset of t, false otherwise
-function propersubset (t)
-  return self:subset (t) and not t:subset (self)
+function propersubset (s, t)
+  return subset (s, t) and not subset (t, s)
 end
 
 -- @func equal: Find whether two sets are equal
---   @param t: set
+--   @param s, t: sets
 -- @returns
 --   @param r: true if sets are equal, false otherwise
-function equal (t)
-  return self:subset (t) and t:subset (self)
+function equal (s, t)
+  return subset (s, t) and subset (s, t)
 end
 
--- Metamethods
+-- @head Metamethods for sets
 -- set + table = union
-getmetatable (_M).__add = union
+metatable.__add = union
 -- set - table = set difference
-getmetatable (_M).__sub = minus
+metatable.__sub = difference
 -- set / table = intersection
-getmetatable (_M).__div = intersect
+metatable.__div = intersection
 -- set <= table = subset
-getmetatable (_M).__le = subset
+metatable.__le = subset
 -- set < table = proper subset
-getmetatable (_M).__lt = propersubset
+metatable.__lt = propersubset
