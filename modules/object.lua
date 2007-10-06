073e3603fc1f662bc3abca9496676d29f114ed1f
rrt <rrt>
2007-10-06 19:06:51 +0000

    Update some comments to match changes in the Lua Gem about this code.

--- modules/object.lua
+++ modules/object.lua
@@ -8,7 +8,7 @@ require "table_ext"
 -- Usage:
 
 -- Create an object/class:
---   object/class = parent {value, ...; field = value ...}
+--   object/class = prototype {value, ...; field = value ...}
 --   An object's metatable is itself.
 --   In the initialiser, unnamed values are assigned to the fields
 --   given by _init (assuming the default _clone).
@@ -16,7 +16,7 @@ require "table_ext"
 
 -- Access an object field: object.field
 -- Call an object method: object:method (...)
--- Call a class method: class.method (self, ...)
+-- Call a class method: Class.method (self, ...)
 
 -- Add a field: object.field = x
 -- Add a method: function object:method (...) ... end
