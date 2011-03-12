d0bfc681e8b7a05e93cabc7f335125476a0601d9
Reuben Thomas <rrt@sc3d.org>
2011-03-12 06:57:02 +0000

    Shorten a TODO.

--- modules/std.lua
+++ modules/std.lua
@@ -8,8 +8,7 @@
 --   error, not die; OO vs non-OO (a thorny problem)
 -- TODO: Add tests for each function immediately after the function;
 --   this also helps to check module dependencies
--- TODO: precompile and make import check for a .lc version of
---   each file, and load it if it's newer than the .lua version.
+-- TODO: pre-compile
 
 module ("std", package.seeall)
 
