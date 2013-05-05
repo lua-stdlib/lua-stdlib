f4b356d7efcebd20fd9a44d87c95e01e00c27ef9
Gary V. Vaughan <gary@gnu.org>
2013-05-05 22:50:01 +0700

    configury: adopt slingshot release mechanism.
    * GNUmakefile, Makefile.am, build-aux/mkrockspecs,
    m4/ax_compare_version.m4, m4/ax_lua.m4, m4/ax_with_prog.m4:
    Remove.  These files are handled by slingshot now.
    * bootstrap: New file. Copied from slingshot subproject.
    * bootstrap.conf (stdlib_copy_slingshot): Install missing slingshot
    files.
    * configure.ac: Adjust to drive slingshot processes.
    * local.mk: New file.  Project local make rules.
    * rockspec.conf: New file.  Slingshot rockspec template.
    * specs/specs.mk: Simplified.
    * .gitignore: Update.
    * .travis.yml: Regenerate.

--- specs/specs.mk
+++ specs/specs.mk
@@ -9,15 +9,6 @@ SPECL_ENV = $(LUA_ENV)
 
 
 ## ------ ##
-## Tools. ##
-## ------ ##
-
-## We're using improved sanity Specl-5 should_error ordering.
-SPECL     ?= specl
-SPECL_MIN  = 5
-
-
-## ------ ##
 ## Specs. ##
 ## ------ ##
 
@@ -31,24 +22,4 @@ specl_SPECS =					\
 	$(srcdir)/specs/table_ext_spec.yaml	\
 	$(NOTHING_ELSE)
 
-check_local += specs-check-local
-specs-check-local:
-	@v=`$(SPECL) --version | sed -e 's|^.* ||' -e 1q`;		\
-	if test "$$v" -lt "$(SPECL_MIN)"; then				\
-	  printf "%s%s\n%s\n"						\
-	    "ERROR: Specl version $$v is too old,"			\
-	    " please upgrade to at least version $(SPECL_MIN),"		\
-	    "ERROR: and rerun \`make check\`";				\
-	  exit 1;							\
-	else								\
-	  $(SPECL_ENV) $(SPECL) $(SPECL_OPTS) $(specl_SPECS);		\
-	fi
-
-
-## ------------- ##
-## Distribution. ##
-## ------------- ##
-
-EXTRA_DIST +=				\
-	$(specl_SPECS)			\
-	$(NOTHING_ELSE)
+include build-aux/specl.mk
