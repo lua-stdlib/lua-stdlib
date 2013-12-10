60bd04d58d18abf5cd361c54e597a88ddd396dc4
Gary V. Vaughan <gary@gnu.org>
2013-12-11 00:07:13 +1300

    set: derive from std.container, keys no longer clash with method names.
    * specs/set_spec.yaml: Replace object methods with prototype
    functions through out.
    Check that set elements with the same name as a prototype
    function behave properly.
    * lib/std/set.lua: Rewrite as a derivative of std.container.
    Change object methods to Container style prototype functions.
    * NEWS: Update.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/set.lua
+++ lib/std/set.lua
@@ -1,10 +1,20 @@
 --[[--
- Set object.
+ Set container.
+
+ Derived from @{std.container}, and inherits Container's metamethods.
+ 
+ Note that Functions listed below are available only available from the
+ Set prototype returned by requiring this module, because Container
+ objects cannot have object methods.
+
  @classmod std.set
+ @see std.container
  ]]
 
-local list   = require "std.base"
-local Object = require "std.object"
+local base      = require "std.base"
+local Container = require "std.container"
+local prototype = (require "std.object").prototype
+
 
 local Set -- forward declaration
 
@@ -12,34 +22,42 @@ local Set -- forward declaration
 -- The representation is a table whose tags are the elements, and
 -- whose values are true.
 
+
 --- Say whether an element is in a set.
+-- @tparam set set a set
 -- @param e element
--- @return `true` if e is in set, `false`
+-- @return `true` if `e` is in `set`, otherwise `false`
 -- otherwise
-local function member (self, e)
-  return rawget (self, e) == true
+local function member (set, e)
+  return rawget (set, e) == true
 end
+
 
 --- Insert an element into a set.
+-- @tparam set set a set
 -- @param e element
 -- @return the modified set
-local function insert (self, e)
-  rawset (self, e, true)
-  return self
+local function insert (set, e)
+  rawset (set, e, true)
+  return set
 end
+
 
 --- Delete an element from a set.
+-- @tparam set set a set
 -- @param e element
 -- @return the modified set
-local function delete (self, e)
-  rawset (self, e, nil)
-  return self
+local function delete (set, e)
+  rawset (set, e, nil)
+  return set
 end
 
+
 --- Iterator for sets.
+-- @tparam set set a set
 -- @todo Make the iterator return only the key
-local function elems (self)
-  return pairs (self)
+local function elems (set)
+  return pairs (set)
 end
 
 
@@ -47,168 +65,211 @@ end
 
 local difference, symmetric_difference, intersection, union, subset, equal
 
+
 --- Find the difference of two sets.
--- @param s set
--- @return `self` with elements of s removed
-function difference (self, s)
-  if Object.type (s) == "table" then
-    s = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return `set1` with elements of s removed
+function difference (set1, set2)
+  if prototype (set2) == "table" then
+    set2 = Set (set2)
   end
   local t = Set {}
-  for e in elems (self) do
-    if not member (s, e) then
+  for e in elems (set1) do
+    if not member (set2, e) then
       insert (t, e)
     end
   end
   return t
 end
+
 
 --- Find the symmetric difference of two sets.
--- @param s set
--- @return elements of `self` and `s` that are in `self` or `s` but not both
-function symmetric_difference (self, s)
-  if Object.type (s) == "table" then
-    s = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return elements of `set1` and `set2` that are in `set1` or `set2` but not both
+function symmetric_difference (set1, set2)
+  if prototype (set2) == "table" then
+    set2 = Set (set2)
   end
-  return difference (union (self, s), intersection (s, self))
+  return difference (union (set1, set2), intersection (set2, set1))
 end
 
+
 --- Find the intersection of two sets.
--- @param s set
--- @return set intersection of `self` and `s`
-function intersection (self, s)
-  if Object.type (s) == "table" then
-    s = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return set intersection of `set1` and `set2`
+function intersection (set1, set2)
+  if prototype (set2) == "table" then
+    set2 = Set (set2)
   end
   local t = Set {}
-  for e in elems (self) do
-    if member (s, e) then
+  for e in elems (set1) do
+    if member (set2, e) then
       insert (t, e)
     end
   end
   return t
 end
 
+
 --- Find the union of two sets.
--- @param s set or set-like table
--- @return set union of `self` and `s`
-function union (self, s)
-  if Object.type (s) == "table" then
-    s = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return set union of `set1` and `set2`
+function union (set1, set2)
+  if prototype (set2) == "table" then
+    set2 = Set (set2)
   end
   local t = Set {}
-  for e in elems (self) do
+  for e in elems (set1) do
     insert (t, e)
   end
-  for e in elems (s) do
+  for e in elems (set2) do
     insert (t, e)
   end
   return t
 end
 
+
 --- Find whether one set is a subset of another.
--- @param s set
--- @return `true` if `self` is a subset of `s`, `false` otherwise
-function subset (self, s)
-  if Object.type (s) == "table" then
-    s = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return `true` if `set1` is a subset of `set2`, `false` otherwise
+function subset (set1, set2)
+  if prototype (set2) == "table" then
+    set2 = Set (set2)
   end
-  for e in elems (self) do
-    if not member (s, e) then
+  for e in elems (set1) do
+    if not member (set2, e) then
       return false
     end
   end
   return true
 end
 
+
 --- Find whether one set is a proper subset of another.
--- @param s set
--- @return `true` if `self` is a proper subset of `s`, `false` otherwise
-function propersubset (self, s)
-  if Object.type (s) == "table" then
-    t = Set (s)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return `true` if `set1` is a proper subset of `set2`, `false` otherwise
+function proper_subset (set1, set2)
+  if prototype (set2) == "table" then
+    t = Set (set2)
   end
-  return subset (self, s) and not subset (s, self)
+  return subset (set1, set2) and not subset (set2, set1)
 end
+
 
 --- Find whether two sets are equal.
--- @param s set
--- @return `true` if `self` and `s` are equal, `false` otherwise
-function equal (self, s)
-  return subset (self, s) and subset (s, self)
+-- @tparam set set1 a set
+-- @tparam table|set set2 another set, or table
+-- @return `true` if `set1` and `set2` are equal, `false` otherwise
+function equal (set1, set2)
+  return subset (set1, set2) and subset (set2, set1)
 end
 
 
-Set = Object {
-  -- Derived object type.
-  _type = "Set",
+--- Set prototype object.
+-- @table std.set
+-- @string[opt="Set"] _type type of Set, returned by
+--   @{std.object.prototype}
+-- @tfield table|function _init a table of field names, or
+--   initialisation function, see @{std.object.__call}
+-- @tfield nil|table|set _functions a table whose keys are the names
+--   of module functions not copied by @{std.object.__call}
+Set = Container {
+  _type      = "Set",
 
-  -- Initialise.
-  _init = function (self, t)
-    for e in list.elems (t) do
-      insert (self, e)
-    end
-    return self
-  end,
+  _init      = function (self, t)
+                 for e in base.elems (t) do
+                   insert (self, e)
+                 end
+                 return self
+               end,
 
+  delete               = delete,
+  difference           = difference,
+  elems                = elems,
+  equal                = equal,
+  insert               = insert,
+  intersection         = intersection,
+  member               = member,
+  proper_subset        = proper_subset,
+  subset               = subset,
+  symmetric_difference = symmetric_difference,
+  union                = union,
 
-  ------
-  -- Union operator.
-  --     set + table = union
+  --- Union operator.
+  --     union = set + table
   -- @function __add
-  -- @param set set
-  -- @param table set or set-like table
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set union
   -- @see union
   __add = union,
 
-  ------
-  -- Difference operator.
-  --     set - table = set difference
+
+  --- Difference operator.
+  --     difference = set - table
   -- @function __sub
-  -- @param set set
-  -- @param table set or set-like table
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set difference
   -- @see difference
   __sub = difference,
 
-  ------
-  -- Intersection operator.
-  --     set * table = intersection
+
+  --- Intersection operator.
+  --     intersection = set * table
   -- @function __mul
-  -- @param set set
-  -- @param table set or set-like table
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set intersection
   -- @see intersection
   __mul = intersection,
 
-  ------
-  -- Symmetric difference operator.
-  --     set / table = symmetric difference
+
+  --- Symmetric difference operator.
+  --     symmetric_difference = set / table
   -- @function __div
-  -- @param set set
-  -- @param table set or set-like table
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set symmetric_difference
   -- @see symmetric_difference
   __div = symmetric_difference,
 
-  ------
-  -- Subset operator.
-  --     set <= table = subset
+
+  --- Subset operator.
+  --     set = set <= table
   -- @function __le
-  -- @param set set
-  -- @param table set or set-like table
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set subset
   -- @see subset
   __le  = subset,
 
-  ------
-  -- Proper subset operator.
-  --     set < table = proper subset
-  -- @function __lt
-  -- @param set set
-  -- @param table set or set-like table
-  -- @see propersubset
-  __lt  = propersubset,
 
-  ------
-  -- Object to table conversion.
-  --     table = set:totable ()
+  --- Proper subset operator.
+  --     proper_subset = set < table
+  -- @function __lt
+  -- @static
+  -- @tparam set set set
+  -- @tparam table|set table another set or table
+  -- @treturn set proper_subset
+  -- @see proper_subset
+  __lt  = proper_subset,
+
+
+  -- Set to table conversion.
+  -- @treturn table table representation of a set.
+  -- @see std.table.totable
   __totable  = function (self)
                  local t = {}
                  for e in elems (self) do
@@ -218,8 +279,9 @@ Set = Object {
                  return t
                end,
 
+
   --- @export
-  __index = {
+  _functions = {
     delete               = delete,
     difference           = difference,
     elems                = elems,
@@ -227,7 +289,7 @@ Set = Object {
     insert               = insert,
     intersection         = intersection,
     member               = member,
-    propersubset         = propersubset,
+    proper_subset        = proper_subset,
     subset               = subset,
     symmetric_difference = symmetric_difference,
     union                = union,
