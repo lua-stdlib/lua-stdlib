e0a69c63821e0ba38c9ff81ebe60e7aa682cd584
Gary V. Vaughan <gary@gnu.org>
2014-04-16 01:18:25 -0700

    base: provide a deprecation mechanism for gentle API upgrades.
    * specs/base_spec.yaml: New file. Specify behaviour for a
    deprecation wrapper.
    * specs/specs.mk (specl_SPECS): Add base_spec.yaml.
    * lib/std/base.lua (deprecate): New function.  Implement the
    specification.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/base.lua
+++ lib/std/base.lua
@@ -1,6 +1,28 @@
 ------
 -- @module std.base
 
+
+--- Write a deprecation warning to stderr on first call.
+-- @func        fn      deprecated function
+-- @string[opt] name    function name for automatic warning message.
+-- @string[opt] warnmsg full specified warning message (overrides *name*)
+-- @return a function to show the warning on first call, and hand off to *fn*
+local function deprecate (fn, name, warnmsg)
+  assert (name or warnmsg,
+          "missing argument to 'deprecate', expecting 2 or 3 parameters")
+  warnmsg = warnmsg or (name .. " is deprecated, and will go away in a future release.")
+  local warnp = true
+  return function (...)
+    if warnp then
+      local _, where = pcall (function () error ("", 4) end)
+      io.stderr:write ((string.gsub (where, "(^w%*%.%w*%:%d+)", "%1")))
+      io.stderr:write (warnmsg .. "\n")
+      warnp = false
+    end
+    return fn (...)
+  end
+end
+
 -- Doc-commented in table.lua...
 local function clone (t, nometa)
   local u = {}
@@ -146,6 +168,7 @@ local M = {
   clone_rename = clone_rename,
   compare      = compare,
   concat       = concat,
+  deprecate    = deprecate,
   elems        = elems,
   ileaves      = ileaves,
   leaves       = leaves,
