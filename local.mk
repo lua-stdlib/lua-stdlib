7f0fa38329967787ddfff96890829eba0e38368e
Gary V. Vaughan <gary@gnu.org>
2014-05-01 17:46:18 +0700

    maint: post-release administrivia.
    * configure.ac (AC_INIT): Bump version to 41.
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * ./local.mk (old_NEWS_hash): Auto-update.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- local.mk
+++ local.mk
@@ -29,7 +29,7 @@ LUA_ENV  = LUA_PATH="$(std_path);$(LUA_PATH)"
 ## Bootstrap. ##
 ## ---------- ##
 
-old_NEWS_hash = 606609f9586288cfe6d9df676719570a
+old_NEWS_hash = b53d7090ac89fb479e5f12aa357d6881
 
 update_copyright_env = \
 	UPDATE_COPYRIGHT_HOLDER='(Gary V. Vaughan|Reuben Thomas)' \
