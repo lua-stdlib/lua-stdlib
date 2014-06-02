35109ea46e4ecc995902b484744509c8dc79bebb
Gary V. Vaughan <gary@gnu.org>
2014-06-02 09:24:39 +0700

    math: complete specs and improve LDocs.
    * lib/std/math.lua (floor, monkey_patch, round): Improve LDocs
    with usage examples.
    * specs/io_spec.yaml (floor, round): Add missing specifications.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/math.lua
+++ lib/std/math.lua
@@ -1,5 +1,6 @@
 --[[--
- Additions to the math module.
+ Additions to the core math module.
+
  @module std.math
 ]]
 
@@ -9,10 +10,10 @@ local _floor = math.floor
 
 
 --- Extend `math.floor` to take the number of decimal places.
--- @function floor
--- @param n number
--- @param p number of decimal places to truncate to (default: 0)
--- @return `n` truncated to `p` decimal places
+-- @number n number
+-- @int[opt=0] p number of decimal places to truncate to
+-- @treturn number `n` truncated to `p` decimal places
+-- @usage tenths = floor (magnitude, 1)
 local function floor (n, p)
   if p and p ~= 0 then
     local e = 10 ^ p
@@ -28,6 +29,7 @@ end
 -- Replaces core `math.floor` with `std.math` version.
 -- @tparam[opt=_G] table namespace where to install global functions
 -- @treturn table the module table
+-- @usage require "std.math".monkey_patch ()
 local function monkey_patch (namespace)
   namespace = namespace or _G
   assert (type (namespace) == "table",
@@ -39,16 +41,17 @@ end
 
 
 --- Round a number to a given number of decimal places
--- @function round
--- @param n number
--- @param p number of decimal places to round to (default: 0)
--- @return `n` rounded to `p` decimal places
+-- @number n number
+-- @int[opt=0] p number of decimal places to round to
+-- @treturn number `n` rounded to `p` decimal places
+-- @usage roughly = round (exactly, 2)
 local function round (n, p)
   local e = 10 ^ (p or 0)
   return _floor (n * e + 0.5) / e
 end
 
 
+--- @export
 local M = {
   floor        = floor,
   monkey_patch = monkey_patch,
