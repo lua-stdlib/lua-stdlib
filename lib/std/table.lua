e2a5a3697024905879bcfb08af9e473f767b4a1c
Gary V. Vaughan <gary@gnu.org>
2014-06-02 09:24:39 +0700

    table: complete specs and improve LDocs.
    * lib/std/table.lua (clone, clone_select, empty, invert, keys)
    (merge, merge_select, monkey_patch, new, pack, ripairs, size)
    (sort, totable, values): Add argcheck calls, and improve LDocs
    with usage examples & cross-references.
    (clone, clone_select, merge, merge_select): Minimise overhead
    when argchecking is disabled by wrapping complex argument
    checking in `if init._ARGCHECK`.
    * specs/table_spec.yaml (pack, ripairs, totable): Add missing
    specifications.
    (clone, clone_select, empty, invert, keys, merge, merge_select)
    (monkey_patch, new, size, sort, values): Improve specs to match
    argcheck behaviours.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/table.lua
+++ lib/std/table.lua
@@ -11,12 +11,13 @@
 ]]
 
 local base = require "std.base"
+local init = require "std.debug_init"
 
 
 local M -- forward declaration
 
 -- No need to pull all of std.list into memory.
-local elems = base.elems
+local argcheck, argscheck, elems = base.argcheck, base.argscheck, base.elems
 
 
 --- Merge one table's fields into another.
@@ -24,7 +25,7 @@ local elems = base.elems
 -- @tparam table u table with fields to merge
 -- @tparam[opt={}] table map table of `{old_key=new_key, ...}`
 -- @tparam boolean nometa if non-nil don't copy metatable
--- @return table   `t` with fields from `u` merged in
+-- @return table *t* with fields from *u* merged in
 local function merge_allfields (t, u, map, nometa)
   map = map or {}
   if type (map) ~= "table" then
@@ -69,12 +70,22 @@ end
 -- To make deep copies, use @{std.tree.clone}.
 -- @tparam table t source table
 -- @tparam[opt={}] table map table of `{old_key=new_key, ...}`
--- @tparam boolean nometa if non-nil don't copy metatable
+-- @tparam[opt] boolean nometa if non-nil don't copy metatable
 -- @return copy of *t*, also sharing *t*'s metatable unless *nometa*
 --   is true, and with keys renamed according to *map*
+-- @see std.table.merge
+-- @see std.table.clone_select
+-- @usage
+-- shallowcopy = clone (original, {rename_this = "to_this"}, ":nometa")
 local function clone (t, map, nometa)
-  assert (type (t) == "table",
-          "bad argument #1 to 'clone' (table expected, got " .. type (t) .. ")")
+  if init._ARGCHECK then
+    local types = {"table", "table", {"boolean", ":nometa", "nil"}}
+    if type (map) ~= "table" then
+      types = {"table", {"table", "boolean", ":nometa", "nil"}}
+    end
+    argscheck ("std.table.clone", types, {t, map, nometa})
+  end
+
   return merge_allfields ({}, t, map, nometa)
 end
 
@@ -102,27 +113,44 @@ local clone_rename = base.deprecate (function (map, t)
 -- @function clone_select
 -- @tparam table t source table
 -- @tparam[opt={}] table keys list of keys to copy
+-- @tparam[opt] boolean nometa if non-nil don't copy metatable
 -- @return copy of fields in *selection* from *t*, also sharing *t*'s
 --   metatable unless *nometa*
+-- @see std.table.clone
+-- @see std.table.merge_select
+-- @usage
+-- partialcopy = clone_select (original, {"this", "and_this"}, true)
 local function clone_select (t, keys, nometa)
-  assert (type (t) == "table",
-          "bad argument #1 to 'clone_select' (table expected, got " .. type (t) .. ")")
+  if init._ARGCHECK then
+    local types = {"table", "table", {"boolean", ":nometa", "nil"}}
+    if type (keys) ~= "table" then
+      types = {"table", {"table", "boolean", ":nometa", "nil"}}
+    end
+    argscheck ("std.table.clone_select", types, {t, keys, nometa})
+  end
+
   return merge_namedfields ({}, t, keys, nometa)
 end
 
 
 --- Return whether table is empty.
 -- @tparam table t any table
--- @return `true` if `t` is empty, otherwise `false`
+-- @return `true` if *t* is empty, otherwise `false`
+-- @usage if empty (t) then error "ohnoes" end
 local function empty (t)
+  argcheck ("std.table.empty", 1, "table", t)
+
   return not next (t)
 end
 
 
 --- Invert a table.
--- @tparam  table t a table with `{k=v, ...}`
--- @treturn table   inverted table `{v=k, ...}`
+-- @tparam table t a table with `{k=v, ...}`
+-- @treturn table inverted table `{v=k, ...}`
+-- @usage values = invert (t)
 local function invert (t)
+  argcheck ("std.table.invert", 1, "table", t)
+
   local i = {}
   for k, v in pairs (t) do
     i[v] = k
@@ -132,9 +160,13 @@ end
 
 
 --- Make the list of keys in table.
--- @tparam  table t any table
--- @treturn table   list of keys
+-- @tparam table t any table
+-- @treturn table list of keys
+-- @see std.table.values
+-- @usage globals = keys (_G)
 local function keys (t)
+  argcheck ("std.table.keys", 1, "table", t)
+
   local l = {}
   for k, _ in pairs (t) do
     l[#l + 1] = k
@@ -147,13 +179,20 @@ end
 -- @tparam table t destination table
 -- @tparam table u table with fields to merge
 -- @tparam[opt={}] table map table of `{old_key=new_key, ...}`
--- @tparam boolean nometa if non-nil don't copy metatable
--- @return table   `t` with fields from `u` merged in
+-- @tparam[opt] boolean nometa if non-nil don't copy metatable
+-- @return table *t* with fields from *u* merged in
+-- @see std.table.clone
+-- @see std.table.merge_select
+-- @usage merge (_G, require "std.debug", {say = "log"}, ":nometa")
 local function merge (t, u, map, nometa)
-  assert (type (t) == "table",
-          "bad argument #1 to 'merge' (table expected, got " .. type (t) .. ")")
-  assert (type (u) == "table",
-          "bad argument #2 to 'merge' (table expected, got " .. type (u) .. ")")
+  if init._ARGCHECK then
+    local types = {"table", "table", "table", {"boolean", ":nometa", "nil"}}
+    if type (map) ~= "table" then
+      types = {"table", "table", {"table", "boolean", ":nometa", "nil"}}
+    end
+    argscheck ("std.table.merge", types, {t, u, map, nometa})
+  end
+
   return merge_allfields (t, u, map, nometa)
 end
 
@@ -164,32 +203,43 @@ end
 -- @tparam table t destination table
 -- @tparam table u table with fields to merge
 -- @tparam[opt={}] table keys list of keys to copy
--- @tparam boolean nometa if non-nil don't copy metatable
+-- @tparam[opt] boolean nometa if non-nil don't copy metatable
 -- @return copy of fields in *selection* from *t*, also sharing *t*'s
 --   metatable unless *nometa*
+-- @see std.table.merge
+-- @see std.table.clone_select
+-- @usage merge_select (_G, require "std.debug", {"say"}, false)
 local function merge_select (t, u, keys, nometa)
-  assert (type (t) == "table",
-          "bad argument #1 to 'merge_select' (table expected, got " .. type (t) .. ")")
-  assert (type (u) == "table",
-          "bad argument #2 to 'merge_select' (table expected, got " .. type (u) .. ")")
+  if init._ARGCHECK then
+    local types = {"table", "table", "table", {"boolean", ":nometa", "nil"}}
+    if type (keys) ~= "table" then
+      types = {"table", "table", {"table", "boolean", ":nometa", "nil"}}
+    end
+    argscheck ("std.table.merge_select", types, {t, u, keys, nometa})
+  end
+
   return merge_namedfields (t, u, keys, nometa)
 end
 
 
 --- Return given metamethod, if any, or nil.
 -- @function metamethod
--- @param x object to get metamethod of
--- @param n name of metamethod to get
--- @return metamethod function or nil if no metamethod or not a
--- function
+-- @tparam std.object x object to get metamethod of
+-- @string n name of metamethod to get
+-- @treturn function|nil metamethod function or `nil` if no metamethod or
+--   not a function
+-- @usage lookup = metamethod (require "std.object", "__index")
 local metamethod = base.metamethod
 
 
 --- Make a table with a default value for unset keys.
--- @param         x default entry value (default: `nil`)
--- @tparam  table t initial table (default: `{}`)
--- @treturn table   table whose unset elements are x
+-- @param[opt=nil] x default entry value
+-- @tparam[opt={}] table t initial table
+-- @treturn table table whose unset elements are x
+-- @usage t = new (0)
 local function new (x, t)
+  argcheck ("std.table.new", 2, {"table", "nil"}, t)
+
   return setmetatable (t or {},
                        {__index = function (t, i)
                                     return x
@@ -206,11 +256,14 @@ end
 
 
 --- An iterator like ipairs, but in reverse.
--- @tparam  table    t any table
--- @treturn function   iterator function
--- @treturn table      the table, `t`
--- @treturn  number    `#t + 1`
+-- @tparam table t any table
+-- @treturn function iterator function
+-- @treturn table the table, *t*
+-- @treturn number `#t + 1`
+-- @usage for i, v = ripairs (t) do ... end
 local function ripairs (t)
+  argcheck ("std.table.ripairs", 1, "table", t)
+
   return function (t, n)
            n = n - 1
            if n > 0 then
@@ -223,8 +276,11 @@ end
 
 --- Find the number of elements in a table.
 -- @tparam table t any table
--- @return number of non-nil values in `t`
+-- @return number of non-nil values in *t*
+-- @usage count = size {foo = true, bar = true, baz = false}
 local function size (t)
+  argcheck ("std.table.size", 1, "table", t)
+
   local n = 0
   for _ in pairs (t) do
     n = n + 1
@@ -237,10 +293,14 @@ end
 local _sort = table.sort
 
 --- Make table.sort return its result.
--- @tparam table    t unsorted table
--- @tparam function c comparator function
--- @return `t` with keys sorted accordind to `c`
+-- @tparam table t unsorted table
+-- @tparam[opt] function c comparator function if passed, otherwise standard
+--   lua `<` operator
+-- @return *t* with keys sorted accordind to *c*
+-- @usage table.concat (sort (object))
 local function sort (t, c)
+  argscheck ("std.table.sort", {"table", "function"}, {t, c})
+
   _sort (t, c)
   return t
 end
@@ -251,20 +311,23 @@ end
 -- Replaces core `table.sort` with `std.table` version.
 -- @tparam[opt=_G] table namespace where to install global functions
 -- @treturn table the module table
+-- @usage local table = require "std.table".monkey_patch ()
 local function monkey_patch (namespace)
+  argcheck ("std.table.monkey_patch", 1, {"table", "nil"}, namespace)
   namespace = namespace or _G
-  assert (type (namespace) == "table",
-          "bad argument #1 to 'monkey_patch' (table expected, got " .. type (namespace) .. ")")
 
   namespace.table.sort = sort
   return M
 end
 
 
---- Turn an object into a table according to __totable metamethod.
--- @tparam  std.object x object to turn into a table
+--- Turn an object into a table according to `__totable` metamethod.
+-- @tparam std.object x object to turn into a table
 -- @treturn table resulting table or `nil`
+-- @usage print (table.concat (totable (object)))
 local function totable (x)
+  argcheck ("std.table.totable", 1, {"object", "table", "string"}, x)
+
   local m = metamethod (x, "__totable")
   if m then
     return m (x)
@@ -281,9 +344,12 @@ end
 
 
 --- Make the list of values of a table.
--- @tparam  table t any table
--- @treturn table   list of values
+-- @tparam table t any table
+-- @treturn table list of values
+-- @see std.table.keys
 local function values (t)
+  argcheck ("std.table.values", 1, "table", t)
+
   local l = {}
   for _, v in pairs (t) do
     l[#l + 1] = v
