dcd8ead44bb4ca62e79e83779044bd3e7b41ba25
Gary V. Vaughan <gary@gnu.org>
2014-06-13 11:37:40 +0700

    refactor: use a function to export io apis.
    * specs/io_spec.yaml (catdir, catfile, die, monkey_patch)
    (process_files, readlines, shell, slurp, splitdir, warn)
    (writelines): Update to latest style.
    Add "too many argument" behaviour checks.
    Simplify and standardise argument error message comparisons.
    * lib/std/io.lua (M): Add module name at element 1.
    (catdir, catfile, die, monkey_patch, process_files, readlines)
    (shell, slurp, splitdir, warn, writelines): Upgrade to
    base.export declarations (for overhead free argcheck calls with
    _DEBUG = false) and simplify accordingly.
    (catdir, catfile, die, monkey_patch, process_files, readlines)
    (shell, slurp, splitdir, warn, writelines): Improve LDocs.
    * NEWS: Update.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/io.lua
+++ lib/std/io.lua
@@ -2,8 +2,8 @@
  Additions to the core io module.
 
  The module table returned by `std.io` also contains all of the entries from
- the core io table.  An hygienic way to import this module, then, is simply
- to override the core `io` locally:
+ the core `io` module table.  An hygienic way to import this module, then,
+ is simply to override core `io` locally:
 
     local io = require "std.io"
 
@@ -11,19 +11,17 @@
 ]]
 
 
-local _ARGCHECK = require "std.debug_init"._ARGCHECK
-
 local base = require "std.base"
 
 local package = {
   dirsep  = string.match (package.config, "^([^\n]+)\n"),
 }
 
-local argcheck, argerror, leaves, split =
-      base.argcheck, base.argerror, base.leaves, base.split
+local argerror, export, leaves, split =
+      base.argerror, base.export, base.leaves, base.split
 
 
-local M -- forward declaration
+local M = { "std.io" }
 
 
 
@@ -52,14 +50,13 @@ end
 
 
 --- Slurp a file handle.
--- @tparam[opt=io.input()] file|string file file handle or name
+-- @function slurp
+-- @tparam[opt=io.input()] file|string file file handle or name;
 --   if file is a file handle, that file is closed after reading
 -- @return contents of file or handle, or nil if error
--- @see std.io.process_files
+-- @see process_files
 -- @usage contents = slurp (filename)
-local function slurp (file)
-  argcheck ("std.io.slurp", 1, "file|string|nil", file)
-
+local slurp = export (M, "slurp (file|string|nil)", function (file)
   local h, err = input_handle (file)
   if h == nil then argerror ("std.io.slurp", 1, err, 2) end
 
@@ -68,17 +65,17 @@ local function slurp (file)
     h:close ()
     return s
   end
-end
+end)
 
 
 --- Read a file or file handle into a list of lines.
--- @tparam[opt=io.input()] file|string file file handle or name
+-- The lines in the returned list are not `\n` terminated.
+-- @function readlines
+-- @tparam[opt=io.input()] file|string file file handle or name;
 --   if file is a file handle, that file is closed after reading
--- @return list of lines
+-- @treturn list lines
 -- @usage list = readlines "/etc/passwd"
-local function readlines (file)
-  argcheck ("std.io.readlines", 1, "file|string|nil", file)
-
+export (M, "readlines (file|string|nil)", function (file)
   local h, err = input_handle (file)
   if h == nil then argerror ("std.io.readlines", 1, err, 2) end
 
@@ -88,17 +85,18 @@ local function readlines (file)
   end
   h:close ()
   return l
-end
+end)
 
 
 --- Write values adding a newline after each.
--- @tparam[opt=io.output()] file h file handle or name
+-- @function writelines
+-- @tparam[opt=io.output()] file h open writable file handle;
 --   the file is **not** closed after writing
--- @param ... values to write (as for write)
+-- @tparam string|number ... values to write (as for write)
 -- @usage writelines (io.stdout, "first line", "next line")
-local function writelines (h, ...)
-  argcheck ("std.io.writelines", 1, "file|string|nil", h)
-
+local writelines = export (M,
+"writelines (file|string|number?, string|number?*)",
+function (h, ...)
   if io.type (h) ~= "file" then
     io.write (h, "\n")
     h = io.output ()
@@ -106,100 +104,79 @@ local function writelines (h, ...)
   for v in leaves (ipairs, {...}) do
     h:write (v, "\n")
   end
-end
+end)
 
 
 --- Overwrite core methods and metamethods with `std` enhanced versions.
 --
--- Adds `readlines` and `writelines` metamethods to core file objects.
+-- Adds @{readlines} and @{writelines} metamethods to core file objects.
+-- @function monkey_patch
 -- @tparam[opt=_G] table namespace where to install global functions
--- @treturn table the module table
+-- @treturn table the `std.io` module table
 -- @usage local io = require "std.io".monkey_patch ()
-local function monkey_patch (namespace)
-  argcheck ("std.io.monkey_patch", 1, "table", namespace)
-
+export (M, "monkey_patch (table?)", function (namespace)
   namespace = namespace or _G
 
-  assert (type (namespace) == "table",
-          "bad argument #1 to 'monkey_patch' (table expected, got " .. type (namespace) .. ")")
-
   local file_metatable = getmetatable (namespace.io.stdin)
-  file_metatable.readlines  = readlines
-  file_metatable.writelines = writelines
+  file_metatable.readlines  = M.readlines
+  file_metatable.writelines = M.writelines
 
   return M
-end
+end)
 
 
 --- Split a directory path into components.
 -- Empty components are retained: the root directory becomes `{"", ""}`.
+-- @function splitdir
 -- @param path path
 -- @return list of path components
--- @see std.io.catdir
+-- @see catdir
 -- @usage dir_components = splitdir (filepath)
-local function splitdir (path)
-  argcheck ("std.io.splitdir", 1, "string", path)
-
+export (M, "splitdir (string)", function (path)
   return split (path, package.dirsep)
-end
+end)
 
 
 --- Concatenate one or more directories and a filename into a path.
+-- @function catfile
 -- @string ... path components
 -- @treturn string path
--- @see std.io.catdir
--- @see std.io.splitdir
+-- @see catdir
+-- @see splitdir
 -- @usage filepath = catfile ("relative", "path", "filename")
-local function catfile (...)
-  local t = {...}
-  if _ARGCHECK then
-    if #t == 0 then
-      argcheck ("std.io.catfile", 1, "string", nil)
-    end
-    for i, v in ipairs (t) do
-      argcheck ("std.io.catfile", i, "string", v)
-    end
-  end
-
-  return table.concat (t, package.dirsep)
-end
+export (M, "catfile (string*)", function (...)
+  return table.concat ({...}, package.dirsep)
+end)
 
 
 --- Concatenate directory names into a path.
--- @param ... path components
+-- @function catdir
+-- @string ... path components
 -- @return path without trailing separator
--- @see std.io.catfile
+-- @see catfile
 -- @usage dirpath = catdir ("", "absolute", "directory")
-local function catdir (...)
-  local t = {...}
-  if _ARGCHECK then
-    for i, v in ipairs (t) do
-      argcheck ("std.io.catdir", i, "string", v)
-    end
-  end
-
-  return (string.gsub (table.concat (t, package.dirsep), "^$", package.dirsep))
-end
+export (M, "catdir (string*)", function (...)
+  return table.concat ({...}, package.dirsep):gsub("^$", package.dirsep)
+end)
 
 
 --- Perform a shell command and return its output.
+-- @function shell
 -- @string c command
 -- @treturn string output, or nil if error
--- @see std.io.slurp
 -- @see os.execute
 -- @usage users = shell [[cat /etc/passwd | awk -F: '{print $1;}']]
-local function shell (c)
-  argcheck ("std.io.shell", 1, "string", c)
-
+export (M, "shell (string)", function (c)
   return slurp (io.popen (c))
-end
+end)
 
 
 ------
--- Signature of `process_files` callback function.
+-- Signature of @{process_files} callback function.
 -- @function process_files_callback
 -- @string[opt] filename filename
 -- @int[opt] i argument number of *filename*
+-- @see process_files
 
 
 --- Process files specified on the command-line.
@@ -208,16 +185,15 @@ end
 -- function. In list of filenames, `-` means `io.stdin`.  If no
 -- filenames were given, behave as if a single `-` was passed.
 -- @todo Make the file list an argument to the function.
--- @tparam process_files_callback fn callback function for each file
+-- @function process_files
+-- @tparam function fn @{process_files_callback} function for each file
 --  argument
--- @see std.io.process_files_callback
+-- @see process_files_callback
 -- @usage #! /usr/bin/env lua
 -- -- minimal cat command
 -- local io = require "std.io"
 -- io.process_files (function () io.write (io.slurp ()) end)
-local function process_files (fn)
-  argcheck ("std.io.process_files", 1, "function", fn)
-
+export (M, "process_files (function)", function (fn)
   -- N.B. "arg" below refers to the global array of command-line args
   if #arg == 0 then
     arg[#arg + 1] = "-"
@@ -230,7 +206,7 @@ local function process_files (fn)
     end
     fn (v, i)
   end
-end
+end)
 
 
 --- Give warning with the name of program and file (if any).
@@ -240,10 +216,11 @@ end
 -- `opts.program` and `opts.line` if any.  @{std.optparse:parse}
 -- returns an `opts` table that provides the required `program`
 -- field, as long as you assign it back to `_G.opts`.
+-- @function warn
 -- @string msg format string
 -- @param ... additional arguments to plug format string specifiers
 -- @see std.optparse:parse
--- @see std.io.die
+-- @see die
 -- @usage
 --   local OptionParser = require "std.optparse"
 --   local parser = OptionParser "eg 0\nUsage: eg\n"
@@ -251,9 +228,7 @@ end
 --   if not _G.opts.keep_going then
 --     require "std.io".warn "oh noes!"
 --   end
-local function warn (msg, ...)
-  argcheck ("std.io.warn", 1, "string", msg)
-
+local warn = export (M, "warn (string, any?*)", function (msg, ...)
   local prefix = ""
   if (prog or {}).name then
     prefix = prog.name .. ":"
@@ -273,38 +248,22 @@ local function warn (msg, ...)
   end
   if #prefix > 0 then prefix = prefix .. " " end
   writelines (io.stderr, prefix .. string.format (msg, ...))
-end
+end)
 
 
 --- Die with error.
 -- This function uses the same rules to build a message prefix
--- as @{std.io.warn}.
+-- as @{warn}.
+-- @function die
 -- @string msg format string
 -- @param ... additional arguments to plug format string specifiers
--- @see std.io.warn
+-- @see warn
 -- @usage die ("oh noes! (%s)", tostring (obj))
-local function die (msg, ...)
-  argcheck ("std.io.die", 1, "string", msg)
-
-  warn (msg, ...)
+export (M, "die (string, any?*)", function (...)
+  warn (...)
   error ()
-end
+end)
 
-
---- @export
-M = {
-  catdir        = catdir,
-  catfile       = catfile,
-  die           = die,
-  monkey_patch  = monkey_patch,
-  process_files = process_files,
-  readlines     = readlines,
-  shell         = shell,
-  slurp         = slurp,
-  splitdir      = splitdir,
-  warn          = warn,
-  writelines    = writelines,
-}
 
 for k, v in pairs (io) do
   M[k] = M[k] or v
