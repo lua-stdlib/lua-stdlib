b836ea3520b1d146a5b3d6ebb0eec1f385d5e175
Gary V. Vaughan <gary@gnu.org>
2014-07-02 17:26:00 +0100

    maint: don't flag `Lua` strings as invalid errors in spec-files.
    * build-aux/sanity-cfg.mk (exclude_file_name_regexp): Add
    specs/debug_spec.yaml.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- build-aux/sanity-cfg.mk
+++ build-aux/sanity-cfg.mk
@@ -1,3 +1,3 @@
-exclude_file_name_regexp--sc_error_message_uppercase = ^lib/std/(list|optparse).lua$$
+exclude_file_name_regexp--sc_error_message_uppercase = ^lib/std/(list|optparse).lua|specs/debug_spec.yaml$$
 
 EXTRA_DIST += build-aux/sanity-cfg.mk
